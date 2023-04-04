46e680f544709bbfe0d0c8052867357849524762
TiagoM13 <thyagomotha2000@gmail.com>
2023-04-04 14:56:10 -0300

    add nestjs config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb://nest-mongodb:f296cd9a@localhost:27017/admin',
-    ),
     TasksModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@localhost:27017/admin`,
+    ),
   ],
   controllers: [],
   providers: [],
